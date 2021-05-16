6baa9f93f1cde76037b535348576cdbf1083c81a
michelml <michmoreau.l@gmail.com>
2021-05-16 15:35:05 -0400

    use oatpp starter
    see for template https://github.com/oatpp/oatpp-starter.git

---
+++ src/controller/MyController.hpp
@@ -0,0 +1,39 @@
+#ifndef MyController_hpp
+#define MyController_hpp
+
+#include "dto/DTOs.hpp"
+
+#include "oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/macro/component.hpp"
+
+#include OATPP_CODEGEN_BEGIN(ApiController) //<-- Begin Codegen
+
+/**
+ * Sample Api Controller.
+ */
+class MyController : public oatpp::web::server::api::ApiController {
+public:
+  /**
+   * Constructor with object mapper.
+   * @param objectMapper - default object mapper used to serialize/deserialize DTOs.
+   */
+  MyController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper))
+    : oatpp::web::server::api::ApiController(objectMapper)
+  {}
+public:
+  
+  ENDPOINT("GET", "/", root) {
+    auto dto = MyDto::createShared();
+    dto->statusCode = 200;
+    dto->message = "Hello World!";
+    return createDtoResponse(Status::CODE_200, dto);
+  }
+  
+  // TODO Insert Your endpoints here !!!
+  
+};
+
+#include OATPP_CODEGEN_END(ApiController) //<-- End Codegen
+
+#endif /* MyController_hpp */
