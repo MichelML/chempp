1b45f9f95fbf909cc237058c518c1ff029fd74c1
michelml <michmoreau.l@gmail.com>
2021-05-22 20:56:05 -0400

    add sim search indexes

--- utility/populate_db.sql
+++ utility/populate_db.sql
@@ -2,7 +2,7 @@ drop database if exists molecules;
 create table raw_data (id SERIAL, smiles text, mcule_id text);
 create extension rdkit;
 copy raw_data(smiles, mcule_id)
-from '/mcule.smi' DELIMITER E'\t' CSV;
+from '/mcule.smi' DELIMITER E '\t' CSV;
 select * into mols
 from (
         select id,
@@ -11,3 +11,14 @@ from (
     ) tmp
 where m is not null;
 create index molidx on mols using gist(m);
+select id,
+    m::text,
+    torsionbv_fp(m) as torsionbv,
+    morganbv_fp(m) as mfp2,
+    featmorganbv_fp(m) as ffp2 into fps
+from mols;
+create index fps_ttbv_idx on fps using gist(torsionbv);
+create index fps_mfp2_idx on fps using gist(mfp2);
+create index fps_ffp2_idx on fps using gist(ffp2);
+alter table fps
+add primary key (id);
