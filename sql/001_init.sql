fb994fc43135eb424956083cf012af0e69f2da4f
michelml <michmoreau.l@gmail.com>
2021-05-22 22:43:26 -0400

    format sql

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -1,15 +1,18 @@
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS AppUser (
-    id                      varchar (256) PRIMARY KEY,
-    username                varchar (256) NOT NULL,
-    email                   varchar (256) NOT NULL,
-    password                varchar (256) NOT NULL,
-    role                    varchar (256) NULL,
-    CONSTRAINT              UK_APPUSER_USERNAME UNIQUE (username),
-    CONSTRAINT              UK_APPUSER_EMAIL UNIQUE (email)
+    id varchar (256) PRIMARY KEY,
+    username varchar (256) NOT NULL,
+    email varchar (256) NOT NULL,
+    password varchar (256) NOT NULL,
+    role varchar (256) NULL,
+    CONSTRAINT UK_APPUSER_USERNAME UNIQUE (username),
+    CONSTRAINT UK_APPUSER_EMAIL UNIQUE (email)
 );
-
-INSERT INTO AppUser
-(id, username, email, password, role) VALUES (uuid_generate_v4(), 'admin', 'admin@domain.com', 'admin', 'ROLE_ADMIN');
+INSERT INTO AppUser (id, username, email, password, role)
+VALUES (
+        uuid_generate_v4(),
+        'admin',
+        'admin@domain.com',
+        'admin',
+        'ROLE_ADMIN'
+    );
